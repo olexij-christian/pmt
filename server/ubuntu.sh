5d32e3b297fde30a25d5bbd409f14d2515d6f673
Oleksii Shevchenko <peace_oleksij@proton.me>
2024-04-07 21:06:52 +0300

    Work with Ubuntu backend part, now can generate package list of repository.

--- server/ubuntu.sh
+++ server/ubuntu.sh
@@ -22,17 +22,33 @@ UBUNTU_NAME=jammy
 
 # Download raw list of packages
 UBUNTU_PKG_LIST_LINK=http://archive.ubuntu.com/ubuntu/dists/$UBUNTU_NAME/main/binary-$ARCH/Packages.gz
-UBUNTU_RAW_PKG_LIST_PATH=$BUILD_DIR/Packages
-UBUNTU_RAW_PKG_LIST_COMMPRESSED_PATH=$UBUNTU_RAW_PKG_LIST_PATH.gz
+UBUNTU_RAW_PKG_LIST=$BUILD_DIR/ubuntu_raw_list
+UBUNTU_RAW_PKG_LIST_COMMPRESSED=$UBUNTU_RAW_PKG_LIST.gz
 
-if [ ! -s "$UBUNTU_RAW_PKG_LIST_PATH" ]; then
+if [ ! -s "$UBUNTU_RAW_PKG_LIST" ]; then
   log::info "Download raw list of packages"
-  curl -sL $UBUNTU_PKG_LIST_LINK -o $UBUNTU_RAW_PKG_LIST_COMMPRESSED_PATH
-  gzip --decompress --stdout $UBUNTU_RAW_PKG_LIST_COMMPRESSED_PATH > $UBUNTU_RAW_PKG_LIST_PATH
-  rm -f $UBUNTU_RAW_PKG_LIST_COMMPRESSED_PATH
+  curl -sL $UBUNTU_PKG_LIST_LINK -o $UBUNTU_RAW_PKG_LIST_COMMPRESSED
+  gzip --decompress --stdout $UBUNTU_RAW_PKG_LIST_COMMPRESSED > $UBUNTU_RAW_PKG_LIST
+  rm -f $UBUNTU_RAW_PKG_LIST_COMMPRESSED
 fi
 
-# Parse raw list to simple list
-UBUNTU_PKG_LIST_PATH=$BUILD_DIR/ubuntu_pkgs
+# Split raw file to (one file) is (one package)
+UBUNTU_PKG_RAW_LIST_DIR=$BUILD_DIR/ubuntu_raw_pkgs
+log::info "Split raw data"
+rm -rf $UBUNTU_PKG_RAW_LIST_DIR
+mkdir $UBUNTU_PKG_RAW_LIST_DIR
+perl -00ne "open(F, \">$UBUNTU_PKG_RAW_LIST_DIR/output$.txt\"); print F \$_; close(F)" $UBUNTU_RAW_PKG_LIST
+
+# Generate list of pkgs
+UBUNTU_PKG_LIST=$BUILD_DIR/ubuntu_pkgs
+rm -f $UBUNTU_PKG_LIST
+for raw_pkg_info in "$UBUNTU_PKG_RAW_LIST_DIR/*"; do
+  cat $raw_pkg_info | grep -Po '(?<=Package: )\S+' >> $UBUNTU_PKG_LIST
+done
+
+# Generate paths of pkgs with name of every pkg
+UBUNTU_PATHS=$BUILD_DIR/ubuntu_paths
+rm -rf $UBUNTU_PATHS
+mkdir $UBUNTU_PATHS
 
 # TODO Wait developer... : )
